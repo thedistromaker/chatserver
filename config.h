8aab64583c50e2b970d8ea91ab7bf87dccc613b4
thedistromaker <pan.savvy.35@gmail.com>
2026-04-06 19:37:10 +0100

    Update config.h

--- config.h
+++ config.h
@@ -2,10 +2,10 @@
 #define CONFIG_H
 
 #define PORT 11468
-#define MAX_MSG_LENGTH 512
-#define MAX_NAME_LENGTH 64
-#define MAX_CLIENTS 15
-#define MAX_TABS 6
-#define MAX_MSGS 100
+#define MAX_MSG_LENGTH 128
+#define MAX_NAME_LENGTH 16
+#define MAX_CLIENTS 25
+#define MAX_TABS 1
+#define MAX_MSGS 150
 
 #endif
